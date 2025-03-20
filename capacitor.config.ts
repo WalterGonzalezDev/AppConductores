5e125aa42bfbc1bfec7b32450a90efd928039244
Walter González <waltergonzalezdeveloper@gmail.com>
2025-03-20 09:34:43 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'AppConductores',
+  webDir: 'www'
+};
+
+export default config;
