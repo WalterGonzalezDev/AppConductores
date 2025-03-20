bca3681b96c898557bc067d73463de50f09bd05e
Walter González <waltergonzalezdeveloper@gmail.com>
2025-03-20 16:01:08 -0500

    Se agrega nuevas dependencias

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,23 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
+import { provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
+import { provideHttpClient } from '@angular/common/http';
+import { RouteReuseStrategy } from '@angular/router';
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
+import { environment } from './environments/environment';
+import { enableProdMode } from '@angular/core';
+
+if (environment.production) {
+  enableProdMode();
+}
 
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
     provideRouter(routes, withPreloading(PreloadAllModules)),
+    provideHttpClient(), // Agrega HttpClient si lo necesitas
   ],
-});
+}).catch((err) => console.error(err));
