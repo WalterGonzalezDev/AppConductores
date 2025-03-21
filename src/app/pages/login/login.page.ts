ea136050185d5202f8ec6a4de494c3e8a2c38f03
Walter González <waltergonzalezdeveloper@gmail.com>
2025-03-21 09:38:12 -0500

    Se configura la página de Login

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,32 +1,26 @@
-import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
-import { ApiService } from 'src/app/services/api.service';
+import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { ApiService } from '../../services/api.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
-  standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
-export class LoginPage implements OnInit {
-  credentials = {
-    email: '', password: ''
-  };
+export class LoginPage {
+  credentials = { email: '', password: '' };
 
-  constructor( private apiService: ApiService, private router: Router) {}
+  constructor(private apiService: ApiService, private router: Router) {}
 
-  login(){
-    this.apiService.login(this.credentials).subscribe((response) => {
-      console.log(response);
-      this.router.navigate(['/home']);
-    });
+  login() {
+    this.apiService.login(this.credentials).subscribe(
+      (response) => {
+        console.log('Login exitoso', response);
+        this.router.navigate(['/home']);
+      },
+      (error) => {
+        console.error('Error en el login', error);
+      }
+    );
   }
-
-  ngOnInit() {
-  }
-
 }
